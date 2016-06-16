871aa24940b76ad28cbe9024161cae515718b92d
Daniel <pianojammer@comcast.net>
2016-06-16 16:45:22 -0700

    Adding code like no tomorrow
    It fills you with determination

---
+++ humidity.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <HDC1050.h>
+
+HDC1050 hdcSensor;
+
+float tc, tf, h;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+
+  Serial.println(hdcSensor.getManufacturerID(), HEX);
+  Serial.println(hdcSensor.getDeviceID(), HEX);
+  Serial.println(hdcSensor.getSerialID());
+  Serial.println(hdcSensor.batteryOK());
+
+  hdcSensor.turnOnHeater(true);
+  hdcSensor.setTemperatureRes(HDC1050::T_RES-14);
+  hdcSensor.setHumidityRes(HDC1050::H_RES_8);
+  hdcSensor.updateConfigRegister();
+}
+
+void loop()
+{
+  tf = hdcSensor.getTemperatureHumidity(tc, h);
+
+  Serial.print(tc);
+  Serial.print(" ");
+  Serial.print(tf);
+  Serial.print(" ");
+  Serial.println(h);
+}
+
